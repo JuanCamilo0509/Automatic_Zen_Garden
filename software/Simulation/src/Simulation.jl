8757a1d49b191e212eff4368e22ef94ea9c6083b
juanCamilo0509 <juank109845@gmail.com>
2024-12-22 15:20:00 -0500

    Feat: Add the time between pins to obtein the angular velocity

--- software/Simulation/src/Simulation.jl
+++ software/Simulation/src/Simulation.jl
@@ -1,18 +1,22 @@
-# TODO: Find the time to change between pins to create a constant velocity in the motor.
-# const unitStep = 5.625 / 64 # Deg/steps
+# const angleStepMc = 5.625 / 64 # Deg/steps with micro steps control
+const angleStep = 5.625 # Deg/steps
 
 const zp, zn = 17, 45
 const relation = zp / zn
 const R = 5 # mm
-const w1 = 2 # deg/s
-const w2 = 1 # deg/s
-Iposition = (0, 0) # Initial position (mm, deg)
+const w1 = 5 # deg/s
+const w2 = 10 # deg/s
+const timesAngleStep = (angleStep / w1, angleStep / w2) # The time between steps to get the require angular velocity for each motor.
+
+Iposition = (10, 4) # Initial position (mm, deg)
 Fposition = (30, 10) # Final position (mm, deg)
 function times(Iposition, Fposition)
   r = Fposition[1] - Iposition[1] # Delta r
-  time_r = (r) / (w2 * R)
+  time_r = (r) / (w1 * R)
   theta = Fposition[2] - Iposition[2] # Delta theta
-  t_theta = theta / (relation * w1)
+  t_theta = theta / (relation * w2)
   return (time_r, t_theta)
 end
-println(times(Iposition, Fposition))
+result = times(Iposition, Fposition)
+println("Time to go to the target position:\nRadial Motor: $(result[1])\nAngular Motor: $(result[2])\n")
+println("Time by step:\nRadial Motor: $(timesAngleStep[1])\nAngular Motor: $(timesAngleStep[2])")
